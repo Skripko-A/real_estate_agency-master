1434f5ee65227f2c19aa9753ff3dffd2d8d94988
Aleksandr Skripko <skripkoae@gmail.com>
2024-06-28 15:40:59 +0300

    remove fields Flat.owner, .owners_phonenumber, owner_pure_phone_number

--- property/admin.py
+++ property/admin.py
@@ -13,8 +13,7 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'price')
     readonly_fields = ('created_at',)
     list_editable = ('new_building',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',
-                    'owners_phonenumber', 'owner_pure_phonenumber')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     inlines = [PropertyOwnerFlats,]
 
